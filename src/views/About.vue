e355a4fff885bcf63e4de5f634dc53f7bfcadbd7
ramzi syuhada <ramzi.syuhada@mhs.itenas.ac.id>
2024-12-15 20:52:22 +0700

    Mengganti tittle

--- src/views/About.vue
+++ src/views/About.vue
@@ -13,7 +13,7 @@
     <!---- <div style="margin-top: 20px;">I've worked on  <router-link to="/game-projects">stuff</router-link>, on <router-link to="/other-projects">other stuff</router-link>, and took part in <router-link to="/resume">a few things</router-link> as well.</div>
 
       <div style="margin-top: 40px;">I'm <strong>currently looking for a job</strong> as a monk, like my good friend John Rambo did a few years back. You can reach me at <a href="mailto:johnmatrix@deltaforce.us">johnmatrix@deltaforce.us</a> or <router-link to="/contact">through here</router-link>.</div>
-    -->
+    --> 
     </div>
 
     <div class="photo">
